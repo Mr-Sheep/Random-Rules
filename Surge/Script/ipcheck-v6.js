2d4754dd0996ba27b3111033708a1ced31eb7a86
Snorlax <snorlax@snorlax.blue>
2022-02-04 18:07:30 +0800

    add IP geo info in panel

---
+++ Surge/Script/ipcheck-v6.js
@@ -0,0 +1,37 @@
+/*
+https://raw.githubusercontent.com/Mr-Sheep/Random-Rules/master/Surge/Script/ipcheck-v6.js
+
+* [Panel]
+* Network Info = script-name=Network Info, title="Network Info", content="Refresh", style=info, update-interval=60
+* ...
+* [Script]
+* Network Info = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/Mr-Sheep/Random-Rules/master/Surge/Script/ipcheck-v6.js
+*/
+
+let url = "https://api-ipv6.ip.sb/geoip"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.ip
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.country_code)
+    let asn = jsonData.asn
+    let asOrg = jsonData.asn_organization
+    
+  body = {
+    title: "Network Info",
+    content: `IPv6: ${ip}\n ASN: ${asn}: ${asOrg}\n Geo: ${emoji}${country}`,
+    icon: "link.icloud",
+    'icon-color': "#5AC8FA"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
